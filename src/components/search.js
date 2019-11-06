36e7a20b84c8ada39a00f37ae5f8a410c743cb3d
Yurii Litovinskyi <y.litovinskyi@gmail.com>
2019-11-06 13:50:24 +0200

    solving a problem with empty input

--- src/components/search.js
+++ src/components/search.js
@@ -2,23 +2,27 @@ import React from 'react';
 import './search.css';
 
 const Search = (props) => {
-	return(		
-		<div className="input-group input-group-lg input-for-book">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="inputGroup-sizing-lg">Enter Book Name</span>
-            </div>
-            <input
-                 onChange={props.handleSearch} 
-                 type="text" 
-                 className="form-control" 
-                 aria-label="Sizing example input" 
-                 aria-describedby="inputGroup-sizing-lg" />
-            <button
-                  onClick={props.searchBook} 
-                  type="button" 
-                  className="btn btn-info btn-search"
-                  >Search</button>
-          </div>		
+	return(	
+    <form onSubmit={props.searchBook}>	
+  		<div className="input-group input-group-lg input-for-book">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="inputGroup-sizing-lg">Enter Book Name</span>
+        </div>
+        <input
+             onChange={props.handleSearch} 
+             type="text" 
+             className="form-control" 
+             required
+             aria-label="Sizing example input" 
+             aria-describedby="inputGroup-sizing-lg" 
+             />
+        <button                   
+              type="submit" 
+              className="btn btn-info btn-search"
+              >Search
+              </button>
+      </div>	
+    </form>	
 	)
 }
 
