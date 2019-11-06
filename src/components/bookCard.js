a7df74b5cd27c216942334adf161a4feb4ee274e
YuriiLitovinskyi <y.litovinskyi@gmail.com>
2019-11-06 21:26:00 +0200

    adding media query, styles

--- src/components/bookCard.js
+++ src/components/bookCard.js
@@ -8,8 +8,9 @@ const BookCard = (props) => {
 		<div className="card">
 			<a href={props.reference} target="_blank" rel="noopener noreferrer">
 			  <img src={props.image === "" ? noImage : props.image} className="card-img-top" alt="Book" />
-			</a>
+			</a>			
 			  <div className="card-body">
+			    <hr />
 			    <h5 className="card-title">{props.title}</h5>
 			    <h6 className="card-text">{props.author}</h6>
 			    <h6 className="card-text">{props.published}</h6>
